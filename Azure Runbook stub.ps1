6dace60e42cabd1903349f4f25d8f2d49394f95a
Nick Wagner <nwagner@getnerdio.com>
2024-06-17 14:57:52 -0500

    adding stub files

---
+++ Azure Runbook stub.ps1
@@ -0,0 +1,25 @@
+#description: A runbook script
+#tags: Preview
+
+<# Notes:
+    This script will do things in Azure!
+#>
+
+<# Variables:
+{
+    "Param1": {
+        "Description": "The first parameter",
+        "IsRequired": true
+    },
+    "Param2": {
+        "Description": "An optional parameter",
+        "IsRequired": false
+    }
+}
+#>
+
+$ErrorActionPreference = 'Stop'
+
+# Your script here
+
+
---
+++ Windows Script stub.ps1
@@ -0,0 +1,12 @@
+#description: Helpful script
+#execution mode: IndividualWithRestart
+#tags: Preview
+
+<# Notes:
+    This script will do some things!
+#>
+
+
+$ErrorActionPreference = 'Stop'
+
+
